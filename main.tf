d92b1e8c5fe75c77bb80563ff7999860318bda06
Nawabu-Sujatha <122606574+Nawabu-Sujatha@users.noreply.github.com>
2023-08-22 15:55:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
 provider "aws" {
-    region = "ap-south-1"  
+        region = "ap-south-1"
 }
-
-resource "aws_instance" "foo" {
-  ami           = "ami-0da59f1af71ea4ad2" # ap-south-1
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
+resource "aws_s3_bucket" "sample"{
+        bucket = "terraformbucket123"
 }
